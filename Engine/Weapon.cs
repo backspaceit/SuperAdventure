d80d9846a76108db876917f73ae52ddfa4bb6915
Carl Dean <carlanthony2@hotmail.com>
2017-04-27 15:48:29 +0100

    UI Changes

--- Engine/Weapon.cs
+++ Engine/Weapon.cs
@@ -8,6 +8,11 @@ namespace Engine
 {
     class Weapon : Item
     {
+        public Weapon(int id, string name, string namePlural) : base(id, name, namePlural)
+        {
+
+        }
+
         public int MinimumDamage { get; set; }
         public int MaximumDamage { get; set; }
     }
